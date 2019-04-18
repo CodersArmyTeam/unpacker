f5798d576b7a3a0045614fa17bddce7d41d25f60
Devourer of Source <panczerwonyser@gmail.com>
2019-04-18 13:12:37 +0200

    File names

--- unpack.py
+++ unpack.py
@@ -2,6 +2,16 @@ import re
 import mmap
 import os
 
+png_names = []
+ogg_names = []
+level_names = []
+
+def save_to_file(filename, content):
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+
+    with open(filename, 'wb') as file:
+        file.write(content)
+
 def log(string):
     print("[UNPACKER] " + string)
 
@@ -17,11 +27,10 @@ def png_files(content):
     for end in p2.finditer(content):
         ends.append(end.end())
 
-    files_count = len(starts)
+    files_count = len(png_names)
 
     for x in range(files_count):
-        result_file = open('GRAPH/' + str(x) + '.png', 'wb')
-        result_file.write(content[starts[x]:ends[x]])
+        save_to_file(png_names[x], content[starts[x]:ends[x]])
     log("UNPACKING PNG FILES: DONE")
 
 def ogg_files(content):
@@ -31,14 +40,16 @@ def ogg_files(content):
     for header in p.finditer(content):
         headers.append(header.start())
 
-    files_count = len(headers)
+    files_count = len(ogg_names)
 
     for x in range(files_count):
-        result_file = open('OGG/' + str(x) + '.ogg', 'wb')
+        #result_file = open('OGG/' + str(x) + '.ogg', 'wb')
         if x < len(headers)-1:
-            result_file.write(content[headers[x]:headers[x+1]-1])
-        else:
-            result_file.write(content[headers[x]:])
+            #result_file.write(content[headers[x]:headers[x+1]-1])
+            save_to_file(ogg_names[x], content[headers[x]:headers[x+1]-1])
+        #else:
+            #save_to_file(ogg_names[x], content[headers[x]:])
+            #result_file.write(content[headers[x]:])
     log("UNPACKING OGG FILES: DONE")
 
 def text(content):
@@ -57,6 +68,8 @@ def file_names(content):
     for x in p.finditer(name_content):
         pos.append(x.start())
 
+    unknown = 0
+
     names = []
     for x in range(len(pos)):
         if x < len(pos)-1:
@@ -64,39 +77,42 @@ def file_names(content):
         else:
             names.append(name_content[pos[x]:])
 
-    print(names)
+    for name in names:
+        if name.find(b".png") > -1:
+            png_names.append(name)
+        elif name.find(b".ogg") > -1:
+            ogg_names.append(name)
+        elif name.find(b".poziom") > -1:
+            level_names.append(name)
+        else:
+            log("UNKNOWN FILE TYPE")
+            unknown += 1
+
+    log("NUMBER OF FILES WITH UNKNOWN EXTENSIONS: " + str(unknown))
 
 def level_files(content):
     log("UNPACKING LEVEL FILES...")
 
     level_content = content[23131321:23435322]
 
-    level_count = int((23435322-23131321)/1152)+1
+    level_count = len(level_names) #int((23435322-23131321)/1152)+1
+    print(level_count)
 
     for x in range(level_count):
-        result_file = open('LEVELS/' + str(x) + '.poziom', 'wb')
         if x < level_count:
-            result_file.write(level_content[x*1152:(x+1)*1152])
+            save_to_file(level_names[x], level_content[x*1152:(x+1)*1152])
         else:
-            result_file.write(level_content[x*1152:])
+            save_to_file(level_names[x], level_content[x*1152:])
 
     log("LEVEL FILES: DONE")
 
-def make_directory():
-    if not os.path.exists("./GRAPH"):
-        os.mkdir("./GRAPH")
-    if not os.path.exists("./OGG"):
-        os.mkdir("./OGG")
-    if not os.path.exists("./LEVELS"):
-        os.mkdir("./LEVELS")
-
 with open('../gamedata', 'r') as file:
     with mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ) as content:
-        make_directory()
-        log("VERSION 0.1.4")
+        log("VERSION 0.1.5")
         log("WORKS ONLY WITH GAMEDATA FROM PB 1.0!!!")
-        #file_names(content)
-        level_files(content)
+        
+        file_names(content)
         text(content)
+        #level_files(content) <-- not work
         png_files(content)
         ogg_files(content)
